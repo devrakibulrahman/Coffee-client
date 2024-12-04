9a3203c161dcd7341058fd99dd82894ebad225f3
devrakibulrahman <devrakibulrahman@gmail.com>
2024-12-04 18:07:10 +0600

    add font family and tailwind based style done on 12/4/2024 at 06:07 pm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        'heading-xs': ['1.5rem', { lineHeight: '2rem' }], // Small screens
+        'heading-sm': ['2rem', { lineHeight: '2.5rem' }], // Medium screens
+        'heading-md': ['2.5rem', { lineHeight: '3rem' }], // Large screens
+        'heading-lg': ['3rem', { lineHeight: '3.5rem' }], // Extra-large screens
+        'subpara-xs': ['1rem', { lineHeight: '1.5rem' }],
+        'subpara-sm': ['1.125rem', { lineHeight: '1.75rem' }],
+        'subpara-md': ['1.25rem', { lineHeight: '2rem' }],
+        'subpara-lg': ['1.5rem', { lineHeight: '2.25rem' }],
+      }
+    },
   },
   plugins: [],
 }
-
