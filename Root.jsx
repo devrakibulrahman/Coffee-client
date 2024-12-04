d3c3e090b0c3e9f58aaaf69b6b5167f34e41f2e7
devrakibulrahman <devrakibulrahman@gmail.com>
2024-12-04 18:49:09 +0600

    added header, outlet, and footer components in the root page done on 12/4/2024 at 06:49 pm

--- Root.jsx
+++ Root.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./src/layouts/Header";
+import Footer from "./src/layouts/Footer";
 
 const Root = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,24 +3,36 @@ import Root from "../../Root";
 import AddCoffeePage from "../pages/AddCoffeePage";
 import UpdateCoffeePage from "../pages/UpdateCoffeePage";
 import CoffeeDetailsPage from "../pages/CoffeeDetailsPage";
+import HomePage from "../pages/HomePage";
+import NotFound from "../components/NotFound";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage></HomePage>
+            },
+            {
+                path: '/add coffee',
+                element: <AddCoffeePage></AddCoffeePage>
+            },
+            {
+                path: '/update coffee',
+                element: <UpdateCoffeePage></UpdateCoffeePage>
+            },
+            {
+                path: '/coffee details',
+                element: <CoffeeDetailsPage></CoffeeDetailsPage>
+            },
+            {
+                path: '*',
+                element: <NotFound></NotFound>
+            }
+        ],
     },
-    {
-        path: '/add coffee',
-        element: <AddCoffeePage></AddCoffeePage>
-    },
-    {
-        path: '/update coffee',
-        element: <UpdateCoffeePage></UpdateCoffeePage>
-    },
-    {
-        path: '/coffee details',
-        element: <CoffeeDetailsPage></CoffeeDetailsPage>
-    }
 ]);
 
 export default router;
