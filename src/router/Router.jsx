4bd200a75cbcd5a38f63243d72c0d4a23c84bcd4
devrakibulrahman <devrakibulrahman@gmail.com>
2024-12-04 18:44:04 +0600

    added route in the router.jsx file and also added root.jsx base structure done on 12/4/2024 at 06:44 pm

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../../Root";
+import AddCoffeePage from "../pages/AddCoffeePage";
+import UpdateCoffeePage from "../pages/UpdateCoffeePage";
+import CoffeeDetailsPage from "../pages/CoffeeDetailsPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: ''
+        element: <Root></Root>,
+    },
+    {
+        path: '/add coffee',
+        element: <AddCoffeePage></AddCoffeePage>
+    },
+    {
+        path: '/update coffee',
+        element: <UpdateCoffeePage></UpdateCoffeePage>
+    },
+    {
+        path: '/coffee details',
+        element: <CoffeeDetailsPage></CoffeeDetailsPage>
     }
 ]);
 
